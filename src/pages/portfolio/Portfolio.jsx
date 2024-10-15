a9be6cde2723f5e4f27842f225d2379764daadf0
Harsh Vyas <harshvyas0803@gmail.com>
2024-10-15 23:16:04 +0530

    added portfolio V2

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
-import Finework from '../../components/finework/Finework';
 import Navbar from '../../components/Navbar/Navbar';
 import './Portfolio.css';
 
 const Portfolio = () => {
   return (
-    <div>
-      <div className='nav-in-portfolio'>
-        <Navbar />
-      </div>
-
-      <div className="cards-in-portfolio">
-        <h1 className="heading">Some of Our Finest Work</h1>
-
-        {/* Gradient line */}
-        <div className="gradient-line"></div>
-
-        {/* Filter Buttons */}
-        <div className="filter-buttons">
-          <Link to="/portfolio/all" className="filter-link">
-            <button className="filter-button">All Works</button>
-          </Link>
-          <Link to="/portfolio/mobileapps" className="filter-link">
-            <button className="filter-button">Mobile Apps</button>
-          </Link>
-          <Link to="/portfolio/webapps" className="filter-link">
-            <button className="filter-button">Web Apps</button>
-          </Link>
-          <Link to="/portfolio/logos" className="filter-link">
-            <button className="filter-button">Logos</button>
-          </Link>
-          <Link to="/portfolio/digitalmarketing" className="filter-link">
-            <button className="filter-button">Digital Marketing</button>
-          </Link>
+    <div className='main-portfolio'>
+      <Navbar />
+      <div className="content-project">
+        <div className="heading-project">
+          <h1>Some of our finest work</h1>
         </div>
 
-        <Finework />
+        {/* Filters Section */}
+        <div className="filters">
+          <span className="filter-item">All works</span>
+          <span className="filter-item">Mobile Apps</span>
+          <span className="filter-item">Web Apps</span>
+          <span className="filter-item">Logos</span>
+          <span className="filter-item">Digital Marketing</span>
+        </div>
       </div>
     </div>
   );
