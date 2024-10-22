e279e3db6cdb2e2b620462d5fe9c001f48a09dae
Harsh Vyas <harshvyas0803@gmail.com>
2024-10-23 04:15:01 +0530

    updated Readmmore in project

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -13,9 +13,9 @@ const Product = () => {
     { img: img1, title: 'Clinic Management software', description: 'Reinvent your clinic and enhance your patient care with 5tech-G. From scheduling appointments to patients registrations, check-in, consultations, prescriptions, billing and reporting. the comprehensive 5tech-G platform enables you to deliver a seamless customer experience to your patients. Here\'s an opportunity to scale up your business.' },
     { img: img2, title: 'Market Pro Mobile App', description: 'Send your business ad via WhatsApp and SMS Automatically on every new call - all incoming, outgoing, missed, dial calls. Can add photos, videos, or PDFs. It is not necessary to save the number for WhatsApp. You can change the ad whenever you want' },
     { img: img3, title: 'Distribution System', description: 'An advanced solution for optimizing goods and services distribution, streamlining supply chain processes, reducing costs, and improving efficiency for businesses through automation and real-time visibility.' },
-    { img: img4, title: 'Smart Factory software', description: 'Smart factory software is a computer program that leverages technologies like IoT, AI, and data analytics to automate and optimize manufacturing processes in a factory. It collects real-time data, enables machine-to-machine communication, predicts maintenance needs, automates workflows.' },
+    { img: img4, title: 'Smart Factory software', description: 'Smart factory software is a computer program that leverages technologies like IoT, AI, and data analytics to automate and optimize manufacturing processes in a factory. It collects real-time data, enables machine-to-machine communication, predicts maintenance needs, automates workflows.                                       ' },
     { img: img5, title: 'ERP system for casting company', description: 'Streamline casting operations with an efficient ERP system managing inventory, production planning, resource allocation, and quality control.' },
-    { img: img6, title: 'Matrimonial app', description: 'A user-friendly mobile application connecting individuals for matrimonial purposes, facilitating profile creation, partner search, and communication for a seamless matchmaking experience.' },
+    { img: img6, title: 'Matrimonial app', description: 'A user-friendly mobile application connecting individuals for matrimonial purposes, facilitating profile creation, partner search, and communication for a seamless matchmaking experience.           ' },
   ];
 
   return (
@@ -27,7 +27,9 @@ const Product = () => {
             <img src={product.img} alt={product.title} className="product_card_image" />
             <h3 className="product_card_title">{product.title}</h3>
             <p className="product_card_description">{product.description}</p>
-            <button className="read_more_button">Read More</button>
+            <button className="read_more ">
+                Read More
+              </button>
           </div>
         ))}
       </div>
